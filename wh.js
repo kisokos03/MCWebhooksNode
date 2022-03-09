4aa8ae969a695b647181e11f03fb6963c1de08b0
kisokos03 <bagimate@gmail.com>
2022-03-09 03:08:26 +0000

    fixed path error which made it remake config file each time in server root

--- wh.js
+++ wh.js
@@ -4,7 +4,7 @@ const { WebhookClient, MessageEmbed } = require("discord.js");
 
 function initMCWebhookJSON() {
     var InitJSON = { "statusMessageID": "none", "membersMessageID": "none", "status": "1", "initialized": "true", "webhookURL": "" };
-    fs.writeFileSync("./MCWebhook.json", JSON.stringify(InitJSON, null, 4));
+    fs.writeFileSync(".\\plugins\\webhooks\\MCWebhook.json", JSON.stringify(InitJSON, null, 4));
     console.log("wrote json file")
 
 
@@ -17,14 +17,14 @@ function initialize(members, fileInit) {
     const wc = new WebhookClient({ url: webhookURL });
 
 
-    var webhookData = JSON.parse(fs.readFileSync(".\\MCWebhook.json"));
+    var webhookData = JSON.parse(fs.readFileSync(".\\plugins\\webhooks\\MCWebhook.json"));
     wc.send({ embeds: [(statusEmbedGenerator({ title: "The server is currently ONLINE", color: "00cc00" }))] }).then(msg => {
         webhookData.statusMessageID = msg.id.toString();
-        fs.writeFileSync("./MCWebhook.json", JSON.stringify(webhookData, null, "  "));
+        fs.writeFileSync(".\\plugins\\webhooks\\MCWebhook.json", JSON.stringify(webhookData, null, "  "));
     });
     wc.send({ embeds: [(statusEmbedGenerator({ title: "Noone's joined yet", color: "212e44" }))] }).then(msg => {
         webhookData.membersMessageID = msg.id.toString();
-        fs.writeFileSync("./MCWebhook.json", JSON.stringify(webhookData, null, "  "));
+        fs.writeFileSync(".\\plugins\\webhooks\\MCWebhook.json", JSON.stringify(webhookData, null, "  "));
         if (members != 0) {
             membersUpdate(members);
         }
@@ -39,10 +39,10 @@ function statusON() {
     console.log("statusOn")
     embed = statusEmbedGenerator({ title: "The server is currently ONLINE", color: "00cc00" })
     const wc = new WebhookClient({ url: webhookURL });
-    var statusMessageID = JSON.parse(fs.readFileSync(".\\MCWebhook.json")).statusMessageID;
+    var statusMessageID = JSON.parse(fs.readFileSync(".\\plugins\\webhooks\\MCWebhook.json")).statusMessageID;
     wc.editMessage(statusMessageID, { embeds: [embed] });
     embed = statusEmbedGenerator({ title: "Noone's joined yet", color: '212e44' })
-    var membersMessageID = JSON.parse(fs.readFileSync(".\\MCWebhook.json")).membersMessageID;
+    var membersMessageID = JSON.parse(fs.readFileSync(".\\plugins\\webhooks\\MCWebhook.json")).membersMessageID;
     wc.editMessage(membersMessageID, { embeds: [embed] })
     console.log("statusOnned");
 }
@@ -51,10 +51,10 @@ function statusOFF() {
     console.log("statusOFF");
     embed = statusEmbedGenerator({ title: "The server is currently OFFLINE", color: "cc0000" });
     const wc = new WebhookClient({ url: webhookURL });
-    var statusMessageID = JSON.parse(fs.readFileSync(".\\MCWebhook.json")).statusMessageID;
+    var statusMessageID = JSON.parse(fs.readFileSync(".\\plugins\\webhooks\\MCWebhook.json")).statusMessageID;
     wc.editMessage(statusMessageID, { embeds: [embed] });
     embed = statusEmbedGenerator({ title: "The server is literally offline", color: 'bc644c' });
-    var membersMessageID = JSON.parse(fs.readFileSync(".\\MCWebhook.json")).membersMessageID;
+    var membersMessageID = JSON.parse(fs.readFileSync(".\\plugins\\webhooks\\MCWebhook.json")).membersMessageID;
     wc.editMessage(membersMessageID, { embeds: [embed] });
     console.log("statusOffed")
 }
@@ -62,7 +62,7 @@ function statusOFF() {
 
 
 function membersUpdate(members) {
-    if (JSON.parse(fs.readFileSync('.\\MCWebhook.json')).status == '1') {
+    if (JSON.parse(fs.readFileSync('.\\plugins\\webhooks\\MCWebhook.json')).status == '1') {
         var membersInt = parseInt(members)
         var title
         switch (membersInt) {
@@ -78,7 +78,7 @@ function membersUpdate(members) {
         }
         embed = statusEmbedGenerator({ title: title, color: '4af1ff' })
         const wc = new WebhookClient({ url: webhookURL })
-        var membersMessageID = JSON.parse(fs.readFileSync('.\\MCWebhook.json'))
+        var membersMessageID = JSON.parse(fs.readFileSync('.\\plugins\\webhooks\\MCWebhook.json'))
             .membersMessageID
         wc.editMessage(membersMessageID, { embeds: [embed] })
     }
@@ -99,7 +99,17 @@ function statusEmbedGenerator({ title, desc, color } = {}) {
     return (embed);
 }
 
+function tempmain() {
+    fs.readdir(testFolder, (err, files) => {
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
+}
+
+
 function main() {
+
     switch (process.argv[2].toString()) {
         case ("init"):
             console.log(process.argv[2]);
@@ -125,68 +135,72 @@ function main() {
 }
 
 
-
-try {
-    //file read
-    if (process.argv.length < 3) {
-        throw 0;
-    } else if (process.argv[2].length == 0) {
-        throw 0;
-    }
-
-    const webhookData = fs.readFileSync(".\\MCWebhook.json");
-    console.log("successfully read file")
-    console.log((JSON.stringify(JSON.parse(webhookData))))
+function errorCheck() {
     try {
-        //json parse
-        webhookURL = JSON.parse(webhookData).webhookURL;
-        console.log("successfully read URL")
-        console.log(webhookURL)
+        //file read
+        if (process.argv.length < 3) {
+            throw 0;
+        } else if (process.argv[2].length == 0) {
+            throw 0;
+        }
+
+        const webhookData = fs.readFileSync(".\\plugins\\webhooks\\MCWebhook.json");
+        console.log("successfully read file")
+        console.log((JSON.stringify(JSON.parse(webhookData))))
         try {
-            //url check
-            console.log("checking url validity")
-            if ((webhookURL) == 0) {
-                console.log("error 1")
-                throw (1)
-            } else if (!(webhookURL.includes("discord.com"))) {
-                console.log("error 2")
-                throw (2)
-            } else {
-                console.log("executing main")
-                main()
+            //json parse
+            webhookURL = JSON.parse(webhookData).webhookURL;
+            console.log("successfully read URL")
+            console.log(webhookURL)
+            try {
+                //url check
+                console.log("checking url validity")
+                if ((webhookURL) == 0) {
+                    console.log("error 1")
+                    throw (1)
+                } else if (!(webhookURL.includes("discord.com"))) {
+                    console.log("error 2")
+                    throw (2)
+                } else {
+                    console.log("executing main")
+                    main()
+                }
+
+            } catch (error) {
+                switch (error) {
+                    //url check
+                    case 1:
+                        console.log("Please set the discord webhook URL in MCWebhooks.json")
+                        break;
+                    case 2:
+                        console.log("Please set a valid webhook URL in MCWebhooks.json")
+                        break;
+                    default:
+                        console.log(error);
+                        break;
+                }
+
             }
 
         } catch (error) {
-            switch (error) {
-                //url check
-                case 1:
-                    console.log("Please set the discord webhook URL in MCWebhooks.json")
-                    break;
-                case 2:
-                    console.log("Please set a valid webhook URL in MCWebhooks.json")
-                    break;
-                default:
-                    console.log(error);
-                    break;
-            }
-
+            //json parse
+            console.log(error);
+            console.log("JSON parse failed, writing new file");
+            initMCWebhookJSON();
         }
-
     } catch (error) {
-        //json parse
-        console.log(error);
-        console.log("JSON parse failed, writing new file");
-        initMCWebhookJSON();
-    }
-} catch (error) {
-    //args
-    if (error == 0) {
-        console.log("no args, exiting")
-        process.exit(1);
-    } else {
-        //file read
-        console.log(error)
-        console.log("file read failed, writing new file")
-        initMCWebhookJSON();
+        //args
+        if (error == 0) {
+            console.log("no args, exiting")
+            process.exit(1);
+        } else {
+            //file read
+            console.log(error)
+            console.log("file read failed, writing new file")
+            initMCWebhookJSON();
+        }
     }
 }
+
+
+errorCheck();
